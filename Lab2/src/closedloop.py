4c6fde1faa7d0c5e4bfb9fc6c0604a941789c894
Panzon1496 <97563760+Panzon1496@users.noreply.github.com>
2022-01-29 15:46:34 -0800

    New closed loop code

--- Lab2/src/closedloop.py
+++ Lab2/src/closedloop.py
@@ -4,6 +4,9 @@
     @details Controller uses the difference of reference and current values to create an error variable. the time 
              difference, and magnitude of error are then used in the update method to return a duty for the motor to be run.
     @author John Bennett
+    @author Daniel Munic
+    @author Rodolfo Diaz
+    
     @date   January 25, 2022
 '''
 
@@ -34,7 +37,10 @@ class ClosedLoop:
         ## @brief Previous error
         self.laste = 0
         
-        
+        '''@ creates an object that will keep track of time and the rate of time it takes to count each tick
+        Also creates two lists, so that position and time can be measured and put into a graph in order
+        to be able to see the PID controller in a visual sense. Create a recording object and initiate to false
+        '''
         self.Time = utime.ticks_ms
         self.to = self.Time()
         self.times = []
@@ -59,13 +65,20 @@ class ClosedLoop:
         ## @brief Actuation signal (in duty cycle) calculation using gains and error values
         actuation_signal = self.Kp*(e) + self.Ki*(self.esum) + self.Kd*(dele)
         
+        
+        ''' A loop that that records the current time and position of the motor 100 times.
+            After each recording, it adds the newest postion and time into the lists and once
+            100 recordings have beeen achieved, it prints all the values of these lists then resets
+            recording object back to false. Then prints the word STOP.
+        '''
         if(len(self.times) < 100 and self.recording):
             self.times.append(utime.ticks_diff(self.Time(),self.to))
             self.positions.append(read)
         elif (self.recording):
             self.print_values()
             self.recording = False
-            
+            print("#STOP#")
+        ## Returns the actuation signal which tells the motor how much duty it should give for the next cycle 
         return self.sat(actuation_signal)
                 
     def sat(self,duty):
@@ -80,19 +93,28 @@ class ClosedLoop:
             return self.satLim[1]
         return duty
 
+
+    ## Function that sets the current postion and time as the starting point and time,
+    ## and then begins to record the new values.
     def set_setpoint(self, point):
         self.setpoint = point
         self.to = self.Time()
         self.record()
 
+    ##  Function that set the gain in controller
     def set_control_gain(self, gain):
         self.Kp = gain
         
+        
+    ## Function that clears the values in the lists, time and position, and set the recording object to true
+    ## which allows the code to begin recording the new values of the positon and time
     def record(self):
         self.times = []
         self.positions = []
         self.recording = True
         
+        
+    ##Function that prints the values of the two lists, postion and time
     def print_values(self):
         for i in range(len(self.times)):
             print(str(self.times[i]) + "," + str(self.positions[i]))
